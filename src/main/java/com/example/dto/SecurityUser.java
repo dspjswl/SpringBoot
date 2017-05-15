cc22dc7f6a1cff88b127a9bb26f74acd69f6ed9d
yuheng.lin <yuheng.lin@hand-china.com>
2017-05-15 09:51:37 +0800

    []--

--- src/main/java/com/example/dto/SecurityUser.java
+++ src/main/java/com/example/dto/SecurityUser.java
@@ -1,56 +1,80 @@
 package com.example.dto;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-
 /**
  * SecurityUser.
  * Created by yuheng.lin
  * Date : 2017/5/11
  */
-public class SecurityUser extends User implements UserDetails {
-
-    public SecurityUser(User user) {
-        if (user != null){
-            this.setUsername(user.getUsername());
-            this.setPassword(user.getPassword());
-        }
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
+public class SecurityUser extends User
+       // implements UserDetails
+{
+//
+//    //必须
+//    private Collection<? extends GrantedAuthority> authorities;
+//    private boolean enabled;
+//
+//    public SecurityUser(User user) {
+//        if (user != null){
+//            this.setUsername(user.getUsername());
+//            this.setPassword(user.getPassword());
+//            this.setRole(user.getRole());
+//            this.setId(user.getId());
+//            this.setAccess(user.getAccess());
+//            this.setEnabled(true);
+//        }
+//    }
+//
+//    public SecurityUser(User user,Collection<? extends GrantedAuthority> authorities) {
+//        if (user != null){
+//            this.setUsername(user.getUsername());
+//            this.setPassword(user.getPassword());
+//            this.setRole(user.getRole());
+//            this.setId(user.getId());
+//            this.setAccess(user.getAccess());
+//            this.authorities = authorities;
+//        }
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return authorities;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return this.getPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return this.getUsername();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return enabled;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return enabled;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return enabled;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return enabled;
+//    }
+//
+//    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+//        this.authorities = authorities;
+//    }
+//
+//    public void setEnabled(boolean enabled) {
+//        this.enabled = enabled;
+//    }
 }
--- src/main/java/com/example/dto/User.java
+++ src/main/java/com/example/dto/User.java
@@ -1,5 +1,6 @@
 package com.example.dto;
 
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
@@ -10,9 +11,16 @@ import javax.persistence.Table;
 @Table(name = "SYS_USER")
 public class User {
 
+    @Id
+    private Long id;
+
     private String username;
 
     private String password;
+
+    private String role;
+
+    private int access;
 
     public String getUsername() {
         return username;
@@ -28,5 +36,29 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getAccess() {
+        return access;
+    }
+
+    public void setAccess(int access) {
+        this.access = access;
     }
 }
