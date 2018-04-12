35ab6cd13c0778876db5c6393c98b845b9a3e1a2
yuheng.lin <yuheng.lin@hand-china.com>
2018-04-12 16:00:17 +0800

    调整页面

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -18,7 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan(basePackages = "com.example.mapper")
 @ServletComponentScan
 @EnableTransactionManagement
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
